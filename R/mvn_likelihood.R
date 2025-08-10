4583e91787e10ffe821e00371c6fab28c78d0839
lijackk <lijack@umich.edu>
2025-08-09 21:22:39 -0400

    Adding roxygen documentation

--- R/mvn_likelihood.R
+++ R/mvn_likelihood.R
@@ -3,17 +3,22 @@
 #' @param gamma The exposure-outcome causal effect parameter
 #' @param tau_mu The variance of the normal distribution underlying mu
 #' @param tau_delta The variance of the normal distributions underlying delta
-#' @param r_mat a matrix of estimated (residual) summary statistics correlations due to sample overlap between each set of GWAS summary statistics. Our method does not provide by default.
+#' @param r_mat a matrix of estimated (residual) summary statistics correlations due to sample overlap between each set of GWAS summary statistics. Our method does not provide a matrix by default, but specifies a simple diagonal matrix in the event of no sample overlap
+#' .
 #' @param is_overlap whether there is any sample overlap between any of the K+1 studies used for summary statistics. Usually this is true due to overlap between the outcome and exposure GWAS in the target population, but we assume no overlap by default.
 #' @param sumstat_beta the GWAS effect size estimates for a single variant across K+1 studies: first, for the outcome in the target population, then the exposure in the target population, then the exposures across K-1 auxiliary populations.
 #' @param sumstat_se the standard errors of the effect size estimates in sumstat_beta
 #'
-#' @returns A log-likelihood
+#' @returns A log-likelihood for the summary statistics of a single variant given some parameters gamma, mu, tau
 #' @export
 #'
 #' @examples
-#' simple_loglik_single(sumstat_beta = c(0,0,0), sumstat_se = c(1,1,1), gamma = 0, tau_mu = 1, tau_delta = 1)
-#' simple_loglik_single(sumstat_beta = c(0.4, 0.6, 0.3), sumstat_se = c(0.1, 0.1, 0.05), is_overlap = TRUE, r_mat = matrix(c(1, 0.2, 0, 0.2, 1, 0, 0, 0, 1), nrow = 3, ncol = 3), gamma = 0.8, tau_mu = 1, tau_delta = 1)
+#' simple_loglik_single(sumstat_beta = c(0,0,0), sumstat_se = c(1,1,1),
+#'                      gamma = 0, tau_mu = 1, tau_delta = 1)
+#' simple_loglik_single(sumstat_beta = c(0.4, 0.6, 0.3), sumstat_se = c(0.1, 0.1, 0.05),
+#'                      is_overlap = TRUE,
+#'                      r_mat = matrix(c(1, 0.2, 0, 0.2, 1, 0, 0, 0, 1), nrow = 3, ncol = 3),
+#'                      gamma = 0.8, tau_mu = 1, tau_delta = 1)
 simple_loglik_single <- function(sumstat_beta, sumstat_se, gamma, is_overlap = FALSE, r_mat = NA, tau_mu, tau_delta) {
   # Initial checks
   if (length(sumstat_beta) != length(sumstat_se)) {
