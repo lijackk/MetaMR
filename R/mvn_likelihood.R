e91aa317b0b624c30db1c940afb3863bb67fe700
lijackk <lijack@umich.edu>
2025-08-11 18:17:02 -0400

    Added full loglikelihood function for simple model

--- R/mvn_likelihood.R
+++ R/mvn_likelihood.R
@@ -5,7 +5,7 @@
 #' @param tau_delta The variance of the normal distributions underlying delta
 #' @param r_mat a matrix of estimated (residual) summary statistics correlations due to sample overlap between each set of GWAS summary statistics. Our method does not provide a matrix by default, but specifies a simple diagonal matrix in the event of no sample overlap
 #' .
-#' @param is_overlap whether there is any sample overlap between any of the K+1 studies used for summary statistics. Usually this is true due to overlap between the outcome and exposure GWAS in the target population, but we assume no overlap by default.
+#' @param is_overlap a boolean whether there is any sample overlap between any of the K+1 studies used for summary statistics. Usually this is true due to overlap between the outcome and exposure GWAS in the target population, but we assume no overlap by default.
 #' @param sumstat_beta the GWAS effect size estimates for a single variant across K+1 studies: first, for the outcome in the target population, then the exposure in the target population, then the exposures across K-1 auxiliary populations.
 #' @param sumstat_se the standard errors of the effect size estimates in sumstat_beta
 #'
@@ -50,4 +50,66 @@ simple_loglik_single <- function(sumstat_beta, sumstat_se, gamma, is_overlap = F
   #Calculating the log-likelihood
   logLik <- -1/2 * (log(det(sigma_j)) + t(sumstat_beta) %*% solve(sigma_j) %*% t(t(sumstat_beta)) + (K+1)*log(2*pi))
   return(logLik)
+}
+
+#' The marginal log-likelihood for the summary statistics of a list of variants under our meta-analysis model, assuming no selection, and i.i.d. normal mu and delta
+#'
+#' @param sumstat_beta_list a list of vectors of GWAS effect size estimates with length K+1: first, for the outcome in the target population, then the exposure in the target population, then the exposures across K-1 auxiliary populations. Each vector in the list represents summary statistics for one of many variants.
+#' @param sumstat_se_list a list of vectors of the standard errors for GWAS effect size estimates in sumstat_beta_list
+#' @param gamma The exposure-outcome causal effect parameter
+#' @param is_overlap a boolean describing whether there is any sample overlap between any of the K+1 studies used for summary statistics. Usually this is true due to overlap between the outcome and exposure GWAS in the target population, but we assume no overlap by default.
+#' @param r_mat_list a list of matrices of estimated (residual) summary statistics correlations due to sample overlap between each set of GWAS summary statistics. Our method does not provide a matrix by default, but specifies a simple diagonal matrix in the event of no sample overlap
+#' @param tau_mu The variance of the normal distribution underlying mu
+#' @param tau_delta The variance of the normal distributions underlying delta
+#'
+#' @returns A log-likelihood for the summary statistics of a set of variants given some parameters gamma, mu, tau
+#' @export
+#'
+#' @examples
+#' simple_loglik_full(sumstat_beta_list = list(c(0,0,0), c(0,0,0), c(0,0,0)),
+#'                      sumstat_se_list = list(c(1,1,1), c(1,1,1), c(1,1,1)),
+#'                      gamma = 0, tau_mu = 1, tau_delta = 1)
+#' simple_loglik_full(sumstat_beta_list = list(c(0.4, 0.6, 0.3), c(0.7,1,0.2), c(0.3,0.36,0.3)),
+#'                      sumstat_se_list = list(c(0.1, 0.1, 0.05), c(0.2, 0.2, 0.1), c(0.1, 0.1, 0.05)),
+#'                      is_overlap = TRUE,
+#'                      r_mat_list <- rep(list(matrix(c(1, 0.2, 0, 0.2, 1, 0, 0, 0, 1), nrow = 3, ncol = 3)), 3),
+#'                      gamma = 0.8, tau_mu = 1, tau_delta = 1)
+simple_loglik_full <- function(sumstat_beta_list, sumstat_se_list, gamma, is_overlap = FALSE, r_mat_list = NA, tau_mu, tau_delta) {
+  #Initial checks - will make more later if necessary
+  if (length(sumstat_beta_list) != length(sumstat_se_list)) {
+    stop("Summary statistic effect size estimates and standard errors imply differing numbers of variants!")
+  }
+  k_counts_beta <- unlist(lapply(sumstat_beta_list, length))
+  k_counts_se <- unlist(lapply(sumstat_se_list, length))
+  unique_k_counts <- unique(c(k_counts_beta, k_counts_se))
+  if (length(unique_k_counts) > 1) {
+    stop("Different variants have summary statistic vectors of different lengths!")
+  }
+
+  if (is_overlap == TRUE & identical(r_mat_list, NA)) {
+    stop("If you assume sample overlap, please provide residual correlation matrices in r_mat_list!")
+  }
+
+
+  print("Checks passed, begin log-likelihood calculation and summation")
+  n <- length(sumstat_beta_list)
+  k <- unique_k_counts - 1
+
+  #If no residual correlation matrix is given due to a lack of overlap, generate diag(k) matrices by default
+  if (is_overlap == FALSE & identical(r_mat_list, NA)) { #the default is a standard diagonal matrix for all matrices
+    diag_mat <- list(diag(k + 1))
+    r_mat_list <- rep(diag_mat, n)
+  }
+
+  loglik_variants <- vector()
+  for (i in 1:n) {
+    loglik_variants[i] <- simple_loglik_single(sumstat_beta = sumstat_beta_list[[i]],
+                                               sumstat_se = sumstat_se_list[[i]],
+                                               is_overlap = TRUE,
+                                               r_mat = r_mat_list[[i]],
+                                               gamma = gamma,
+                                               tau_mu = tau_mu,
+                                               tau_delta = tau_delta)
+  }
+  return(sum(loglik_variants))
 }
